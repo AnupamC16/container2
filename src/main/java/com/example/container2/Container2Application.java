e340867171dbd6bee0f9ffb19829a99c2ccc960b
Anupam Chopra <anupamchopra@Anupams-MacBook-Air.local>
2025-03-21 14:58:38 -0300

    Check

--- src/main/java/com/example/container2/Container2Application.java
+++ src/main/java/com/example/container2/Container2Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Container2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Container2Application.class, args); // //
+		SpringApplication.run(Container2Application.class, args); // // //
 	}
 
 }
